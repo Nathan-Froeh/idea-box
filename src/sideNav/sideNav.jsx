5aa00f7e69712b7d935b742360bf959fd7e1bd43
Nathan <nathan.froeh@gmail.com>
2021-03-31 22:13:13 -0500

    chose chakra-ui library

---
+++ src/sideNav/sideNav.jsx
@@ -0,0 +1,11 @@
+import './sideNav.scss';
+
+function SideNav() {
+  return (
+    <div class="side-nav">
+
+    </div>
+  )
+}
+
+export default SideNav;
---
+++ src/topNav/topNav.jsx
@@ -0,0 +1,11 @@
+import './topNav.scss';
+
+function TopNav() {
+  return (
+    <div class="top-nav">
+
+    </div>
+  )
+}
+
+export default TopNav;
