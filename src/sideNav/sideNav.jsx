f4c596cc2ae57e26697e559a3a2f2e39a8993486
Nathan <nathan.froeh@gmail.com>
2021-04-11 21:32:26 -0500

    add inputs

--- src/sideNav/sideNav.jsx
+++ src/sideNav/sideNav.jsx
@@ -2,6 +2,8 @@ import './sideNav.scss';
 import { 
   Box,
   Button,
+  Input,
+  Textarea,
 } from "@chakra-ui/react"
 
 export function SideNav() {
@@ -9,6 +11,10 @@ export function SideNav() {
     <Box className="side-nav" w='300px'>
       
 
+      <Box>
+        <Input placeholder="Title" /> 
+        <Textarea placeholder="Message" /> 
+      </Box>
       <Box>
         <Button colorScheme="blue" variant="outline">Clear</Button>
         <Button colorScheme="blue" variant="solid">Create</Button>
