2454762c8616dc00ca19a6c87995ef1684072acb
Nathan <nathan.froeh@gmail.com>
2021-04-13 19:47:44 -0500

    Add/implement formik for reactive forms

--- src/sideNav/sideNav.jsx
+++ src/sideNav/sideNav.jsx
@@ -3,22 +3,58 @@ import {
   Box,
   Button,
   Input,
-  Textarea,
+  // Textarea,
+  FormControl,
+  FormLabel,
+  FormErrorMessage,
+  
 } from "@chakra-ui/react"
+import { Formik, Field, Form } from 'formik';
+
 
 export function SideNav() {
+
+  function required(value) {
+    let error = '';
+    if (!value) {
+      error = "Required"
+    } 
+    return error
+  }
+
   return (
     <Box className="side-nav" w='300px'>
-      
-
-      <Box>
-        <Input placeholder="Title" /> 
-        <Textarea placeholder="Message" /> 
-      </Box>
-      <Box>
-        <Button colorScheme="blue" variant="outline">Clear</Button>
-        <Button colorScheme="blue" variant="solid">Create</Button>
-      </Box>
+      <Formik
+        initialValues={{ name: "Sasuke" }}
+        onSubmit={(values, actions) => {
+          setTimeout(() => {
+            alert(JSON.stringify(values, null, 2))
+            actions.setSubmitting(false)
+          }, 1000)
+        }}
+      >
+      {(props) => (
+        <Form>
+          <Field name="title" validate={required}>
+            {({ field, form }) => (
+              <FormControl isInvalid={form.errors.title && form.touched.title}>
+                <FormLabel>Title</FormLabel>
+                <Input {...field} id="title" placeholder="title" />
+                <FormErrorMessage>{form.errors.title}</FormErrorMessage>
+              </FormControl>
+            )}
+          </Field>
+          <Button
+            mt={4}
+            colorScheme="teal"
+            isLoading={props.isSubmitting}
+            type="submit"
+          >
+            Submit
+          </Button>
+        </Form>
+      )}
+    </Formik>
     </Box>
   )
 }
