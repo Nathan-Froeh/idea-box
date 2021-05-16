b91e47be9c0ea607fe84f64ca6f462eaf545c6dd
Nathan <nathan.froeh@gmail.com>
2021-05-16 15:13:21 -0500

    delete idea

--- src/card/card.jsx
+++ src/card/card.jsx
@@ -1,5 +1,5 @@
 import './card.scss';
-import React, { useContext } from 'react';
+import React, { useContext, useState } from 'react';
 import { Context } from '../Store/Store';
 import {
   Box,
@@ -7,6 +7,13 @@ import {
   Text,
   IconButton,
   Tooltip,
+  AlertDialog,
+  AlertDialogOverlay,
+  AlertDialogContent,
+  AlertDialogHeader,
+  AlertDialogBody,
+  AlertDialogFooter,
+  Button,
 } from "@chakra-ui/react";
 import {
   StarIcon,
@@ -16,10 +23,18 @@ import {
 
 
 export function Card(props) {
+  console.log('props', props)
   const {dispatch} = useContext(Context);
   const {card} = props;
-  console.log('props', props)
   const yellow = "#FFD712";
+
+  const [isOpen, setIsOpen] = useState(false)
+  const onClose = () => {
+    setIsOpen(false);
+    dispatch({type: 'DELETE-IDEA', payload: card})
+  }
+  const cancelRef = React.useRef()
+
   const starColor = () => {
     if(card.favorite) return yellow
     return 'black'
@@ -30,28 +45,57 @@ export function Card(props) {
   }
 
   return (
-    <Box h="200px" w="200px" p="5px 10px" m="20px" borderWidth="1px" borderRadius="lg" overflow="hidden">
-      <Box w="100%" h="30px" display="flex" flexDirection="row" justifyContent="space-between">
-        <Heading as="h3" size="l">{card.title}</Heading>
-        <Tooltip label="Favorite"> 
-          <IconButton icon={<StarIcon/>} size="xs" color={starColor()} onClick={() => toggleFavorite()}></IconButton>
-        </Tooltip>
+    <>
+      <Box h="200px" w="200px" p="5px 10px" m="20px" borderWidth="1px" borderRadius="lg" overflow="hidden">
+        <Box w="100%" h="30px" display="flex" flexDirection="row" justifyContent="space-between">
+          <Heading as="h3" size="l">{card.title}</Heading>
+          <Tooltip label="Favorite"> 
+            <IconButton icon={<StarIcon/>} size="xs" color={starColor()} onClick={() => toggleFavorite()}></IconButton>
+          </Tooltip>
+        </Box>
+
+        <Box w="100%" h="calc(100% - 54px)" overflow="scroll">
+          <Text>{card.message}</Text>
+          
+        </Box>
+
+        <Box w="100%" h="24px" display="flex" flexDirection="row" justifyContent="space-between">
+          <Tooltip label="Edit">
+            <IconButton icon={<EditIcon/>} size="xs"></IconButton>
+          </Tooltip>
+          <Tooltip label="Delete"> 
+            <IconButton icon={<DeleteIcon/>} size="xs" onClick={() => setIsOpen(true)}></IconButton>
+          </Tooltip>
+
+        </Box>
       </Box>
 
-      <Box w="100%" h="calc(100% - 54px)" overflow="scroll">
-        <Text>{card.message}</Text>
-        
-      </Box>
+      <AlertDialog
+        isOpen={isOpen}
+        leastDestructiveRef={cancelRef}
+        onClose={onClose}
+      >
+        <AlertDialogOverlay>
+          <AlertDialogContent>
+            <AlertDialogHeader fontSize="lg" fontWeight="bold">
+              Delete Idea
+            </AlertDialogHeader>
 
-      <Box w="100%" h="24px" display="flex" flexDirection="row" justifyContent="space-between">
-        <Tooltip label="Edit">
-          <IconButton icon={<EditIcon/>} size="xs"></IconButton>
-        </Tooltip>
-        <Tooltip label="Delete"> 
-          <IconButton icon={<DeleteIcon/>} size="xs"></IconButton>
-        </Tooltip>
+            <AlertDialogBody>
+              Are you sure? You can't undo this action afterwards.
+            </AlertDialogBody>
 
-      </Box>
-    </Box>
+            <AlertDialogFooter>
+              <Button ref={cancelRef} onClick={onClose}>
+                Cancel
+              </Button>
+              <Button colorScheme="red" onClick={onClose} ml={3}>
+                Delete
+              </Button>
+            </AlertDialogFooter>
+          </AlertDialogContent>
+        </AlertDialogOverlay>
+      </AlertDialog>
+    </>
   )
 }
